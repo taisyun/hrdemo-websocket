939f141ba6fe5744cc015e52424641d87df123a5
Taishun Nakatani <nakatani@diva.co.jp>
2016-02-16 10:46:05 +0900

    redux-node-logger

--- src/server/store.js
+++ src/server/store.js
@@ -1,54 +1,19 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import createNodeLogger from 'redux-node-logger'
 import todoApp from './reducer'
 import initialData from './data.json'
+
 
 export default function configureStore(initialState) {
 
   const state = Object.assign({}, initialState, {
     jobList: {
       items: initialData
-/*
-      items: [
-
-        {
-          jobId: 1,
-          code: "JOB0001",
-          name: "Able Seamen",
-          version: 1,
-          _links: {
-            self: {
-              href: "http://192.168.56.101:8091/hrdemo/jobs/1"
-            }
-          }
-        },
-        {
-          jobId: 2,
-          code: "JOB0002",
-          name: "Account Manager",
-          version: 1,
-          _links: {
-            self: {
-              href: "http://192.168.56.101:8091/hrdemo/jobs/2"
-            }
-          }
-        },
-        {
-          jobId: 3,
-          code: "JOB0003",
-          name: "Accountant",
-          version: 1,
-          _links: {
-            self: {
-              href: "http://192.168.56.101:8091/hrdemo/jobs/3"
-            }
-          }
-        },
-
-      ]
-*/
     }
   })
-  const store = createStore(todoApp, state)
+  const loggerMiddleware = createNodeLogger()
+  const enhancer = applyMiddleware(loggerMiddleware)
+  const store = createStore(todoApp, state, enhancer)
 
   return store
 }
